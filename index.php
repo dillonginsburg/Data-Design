e2ec33b0fe59214e8e0eab092a3129f23eeedef9
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-09 16:19:07 -0600

    initial commit data design

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Data Design</title>
+	</head>
+
+	<body>
+		<h1>Data Design Project</h1>
+		<h2>User Persona</h2>
+
+	</body>
+</html>
+
