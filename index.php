3d53f352e5c0f5ad6658c46002628927fce57cd9
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-09 16:54:53 -0600

    2nd commit data design

--- index.php
+++ index.php
@@ -8,7 +8,19 @@
 	<body>
 		<h1>Data Design Project</h1>
 		<h2>User Persona</h2>
+			<p>This is Jonathon Tully. Jonathon tinkers with Tesla cars (repairs and modifications) in his free time.
+				Because Tesla does not make <em>any</em> of their information public, Jon needs to be able to connect with
+				other Tesla hobbyists. Jon can't find any forums for this, and decides to make a subreddit for it.
+			</p>
 
+		<h2>About Jon</h2>
+			<ul><li>Age: 38</li>
+				<li>Gender: Male</li>
+				<li>Ethnicity: Black</li>
+				<li>Occupation: Hardware specialist for dell</li>
+				<li>Equipment: Custom built p.c., Galaxy phone</li>
+				<li>Personality Type: ENFJ</li>
+				</ul>
 	</body>
 </html>
 
