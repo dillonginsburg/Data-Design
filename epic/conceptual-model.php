431912cb5498533ef7ace2e0f468aad0f3999f29
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-11 16:32:02 -0600

    commit 4 data design

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -7,13 +7,8 @@
 	<h1 align="center">Concept Model</h1>
 	<body>
 		<h2>Entities and Attributes</h2>
-		<ul>
-			<h3>User Profile</h3>
-			<li>"karma points</li>
-			<li></li>
 
-		</ul>
-
+		<img src="Capture.PNG">
 	</body>
 </html>
 
