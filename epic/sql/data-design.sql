d85ecc6c523800ab8a64d607a5bf7005903a86da
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-17 16:58:14 -0600

    commit 10ish data design

--- epic/sql/data-design.sql
+++ epic/sql/data-design.sql
@@ -2,17 +2,18 @@ drop table if exists post;
 drop table if exists subreddit;
 drop table if exists user;
 
-create table user (
-	userId binary(16) not null,
-	userEmail varchar(128) not null,
-	index(userId),
-	primary key(userId)
-);
+		create table user (
+		userId binary(16) not null,
+		userEmail varchar(128) not null,
+		index(userId),
+		primary key(userId)
+		);
+
 
 create table subreddit (
-   subredditId binary(16) not null,
-	subredditDetails varchar(2500) not null,
-	subredditRules varchar(2500) not null,
+	subredditId binary(16) not null,
+	subredditDetails blob not null,
+	subredditRules blob not null,
 	subredditUserId binary(16) not null,
 	primary key(SubredditId),
 	foreign key(subredditUserId) references user(userId)
@@ -22,9 +23,9 @@ create table post (
 	postId binary(16) not null,
 	postUserId binary(16) not null,
 	postSubredditId binary(16),
-	postContent varchar(2500) not null,
-	postDate binary(16) not null,
-	postTitle varchar(2500) not null,
+	postContent blob not null,
+	postDate datetime not null,
+	postTitle varchar(128) not null,
 	primary key(postId),
 	foreign key(postSubredditId) references subreddit(subredditId),
 	foreign key(PostUserId) references user(userId)
